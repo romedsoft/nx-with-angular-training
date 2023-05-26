3347ea3a8821011c3db9fe78ed2e17588de79e01
Roman Rodriguez <romedsoft@gmaill.com>
2023-05-26 10:37:39 -0600

    adding admin project and home and product list components

--- romedsoft/apps/admintraining/src/app/app.module.ts
+++ romedsoft/apps/admintraining/src/app/app.module.ts
@@ -4,11 +4,9 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent],
+    declarations: [AppComponent, NxWelcomeComponent],
     imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
     providers: [],
     bootstrap: [AppComponent]
