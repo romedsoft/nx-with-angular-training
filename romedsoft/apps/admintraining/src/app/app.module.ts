5e3aeb6437fcb024b97b8157b37ea21f3711719b
Roman Rodriguez <romedsoft@gmaill.com>
2023-05-29 16:33:21 -0600

    adding components to the admin project

--- romedsoft/apps/admintraining/src/app/app.module.ts
+++ romedsoft/apps/admintraining/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent],
+    declarations: [AppComponent, ShellComponent, SidebarComponent, DashboardComponent],
     imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    exports: [
+      ShellComponent,
+      SidebarComponent,
+      DashboardComponent
+    ]
 })
 export class AppModule {}
